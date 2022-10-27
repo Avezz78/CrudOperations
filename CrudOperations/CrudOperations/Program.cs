6a1231820272528f56b8e3d4d256c548283151d5
Avezz78 <mohammeda@moback.com>
2022-10-27 08:46:13 +0530

    assignment

---
+++ CrudOperations/CrudOperations/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            operations op = new operations();
+            op.CreateUser();
+            op.RetriveUser();
+            op.UpdateUser();    
+            op.DeleteUser();     
+        }
+    }
+}
